0deb4ba21509889ecf55b888bf1eda062d6252ca
EduNagado <eduardo.nagado@gmail.com>
2024-06-03 19:09:22 -0300

    Fazendo as perguntas do Quiz

---
+++ perguntas.js
@@ -0,0 +1,65 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Qual das seguintes opções é uma fonte comum de poluição do ar em áreas urbanas?",
+        answer: "A) Centrais hidroelétricas",
+        answer: "B) Emissões de veículos",
+        options: [
+           "A) Centrais hidroelétricas" ,
+            "B) Emissões de veículos" ,
+            "C) Plantação de árvores",
+           " D) Usinas de energia solar"
+
+        ]
+    },
+    {
+        numb: 2,
+        question: "Qual é o principal componente dos resíduos plásticos encontrados nos oceanos?",
+        answer: "A) Polietileno",
+        answer: "B) Vidro",
+        options: [
+        "A) Polietileno",
+        "B) Vidro",
+        "C) Alumínio",
+        "D) Papelão" ,  
+        ]
+    },
+    {
+        numb: 3,
+        question: "Qual das seguintes práticas ajuda a reduzir a poluição do solo?",
+        answer: "A) Uso de pesticidas em excesso",
+        answer: "B) Descarte inadequado de lixo eletrônico",
+        options: [
+        "A) Uso de pesticidas em excesso",
+        "B) Descarte inadequado de lixo eletrônico",
+        "C) Compostagem de resíduos orgânicos",
+        "D) Queima de resíduos agrícolas",
+
+        ]
+    },
+    {
+        numb: 4,
+        question: "Qual das seguintes alternativas é um exemplo de energia renovável?",
+        answer: "A) Carvão",
+        answer: "B) Gás natural",
+        options: [
+        "A) Carvão",
+        "B) Gás natural",
+        "C) Energia eólica",
+        "D) Petróleo",
+        ]
+    },
+    {
+        numb: 5,
+        question: "Qual é uma das consequências da poluição da água em ecossistemas aquáticos?",
+        answer: "A) Aumento da biodiversidade",
+        answer: "B) Crescimento excessivo de algas (eutrofização)",
+        options: [
+            "A) Aumento da biodiversidade",
+            "B) Crescimento excessivo de algas (eutrofização)",
+            "C) Melhoria na qualidade da água potável",
+            "D) Expansão dos habitats de peixes /// COloque todas as repostas entre ",
+
+        ]
+    }
+];
